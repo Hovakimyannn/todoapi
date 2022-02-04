25a0b0ef88ab920476caeade25a7844e456190c0
Hovakimyannn <harutyunhovakimyan7@gmail.com>
2022-02-04 15:42:11 +0400

    add api

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/get-all', [TodoController::class,'getAll']);
+Route::post('/store',[TodoController::class, 'store']);
+Route::patch('/update',[TodoController::class,'update']);
+Route::delete('/remove',[TodoController::class,'remove']);
